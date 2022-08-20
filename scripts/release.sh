5c3d22d43a79dccb950fd3bedf06449cffa1f193
nikit <nicitazaletin@yandex.ru>
2022-08-21 02:58:47 +0300

    test without perenos stroki

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,7 +11,7 @@ COMMITS="Bad request"
 if git show-ref --tags "$(git describe --tags --abbrev=0 "${RELEASE_VERSION}^" --match="v_*")" --quiet; then
 	echo "i am in if"
 	TAGS_BEFORE_LAST=$(git describe --tags --abbrev=0 "${RELEASE_VERSION}^" --match="v_*")
-	COMMITS=$(git log --pretty=format:"%H %an %s" "v_0.0.70"..."${RELEASE_VERSION}")
+	COMMITS=$(git log --pretty=oneline:"%H %an %s" "v_0.0.70"..."${RELEASE_VERSION}")
 else
   	echo "i am in else"
     	COMMITS=$(git log --pretty=format:"%H %an %s" "${RELEASE_VERSION}")
