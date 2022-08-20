1bb2be336450c589994edc06d586f3fb7f7c8771
nikit <nicitazaletin@yandex.ru>
2022-08-21 00:19:47 +0300

    test in ticket

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,30 +2,31 @@
 
 #echo "${RELEASE_VERSION}"
 
-VERSION=$(git tag --sort version:refname | tail -1 | head -1)
-PREVIOUS_VERSION=$(git tag --sort version:refname | tail -2 | head -1)
-echo "Current version: ${VERSION}"
-echo "Previous version: ${PREVIOUS_VERSION}"
-
-AUTHOR=$(git show "${VERSION}" --pretty=format:"%an" --no-patch)
-DATE=$(git show "${VERSION}" --pretty=format:"%ad" --no-patch)
-echo "${AUTHOR}: ${DATE}"
-
-CHANGELOG=$(git log "$PREVIOUS_VERSION".. --pretty=format:"%s | %an, %ad" --date=short)
-SUMMARY="${VERSION}: ${AUTHOR}, ${DATE}"
-echo "\nChangelog:\n${CHANGELOG}\n"
+#VERSION=$(git tag --sort version:refname | tail -1 | head -1)
+#PREVIOUS_VERSION=$(git tag --sort version:refname | tail -2 | head -1)
+#echo "Current version: ${VERSION}"
+#echo "Previous version: ${PREVIOUS_VERSION}"
+#
+#AUTHOR=$(git show "${VERSION}" --pretty=format:"%an" --no-patch)
+#DATE=$(git show "${VERSION}" --pretty=format:"%ad" --no-patch)
+#echo "${AUTHOR}: ${DATE}"
+#
+#CHANGELOG=$(git log "$PREVIOUS_VERSION".. --pretty=format:"%s | %an, %ad" --date=short)
+#SUMMARY="${VERSION}: ${AUTHOR}, ${DATE}"
+#echo "\nChangelog:\n${CHANGELOG}\n"
 
 CREATE_TASK_URL="https://api.tracker.yandex.net/v2/issues/INFRA-46"
-UNIQUE_KEY="[wignorbo] release ${VERSION}"
+#UNIQUE_KEY="[wignorbo] release ${VERSION}"
 
 HEADER_OAUTH="Authorization: OAuth ${ACCESS_TOKEN}"
 HEADER_HOST="Host: https://api.tracker.yandex.net"
 HEADER_ORG_ID="X-Org-ID: ${ORG_ID}"
 HEADER_CONTENT_TYPE="Content-Type: application/json"
-
+#  "summary": "'"${SUMMARY}"'",
+#  "description": "'"${CHANGELOG}"'"
 REQUEST='{
-  "summary": "'"${SUMMARY}"'",
-  "description": "'"${CHANGELOG}"'"
+    "summary": "Новое название задачи",
+    "description": "Новое описание задачи"
 }'
 echo "Request: ${REQUEST}"
 
