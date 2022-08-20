f6fe6cdda5824df15cae050c44194a13d39beaa4
nikit <nicitazaletin@yandex.ru>
2022-08-21 02:39:04 +0300

    test without perenos stroki

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,7 +14,7 @@ if git show-ref --tags "$(git describe --tags --abbrev=0 "${RELEASE_VERSION}^" -
 	COMMITS=$(git log --pretty=format:"%H %an %s%n" "v_0.0.70"..."${RELEASE_VERSION}")
 else
   	echo "i am in else"
-    	COMMITS=$(git log --pretty=format:"%H %an %s%n" "${RELEASE_VERSION}")
+    	COMMITS=$(git log --pretty=format:"%H %an %s" "${RELEASE_VERSION}")
 fi
 
 #COMMITS=$(git log --pretty=format:"%H %an %s%n" v_0.0.52...${RELEASE_VERSION})
