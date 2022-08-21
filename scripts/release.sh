05470855f822784638edfb9222fc77980b28d817
nikit <nicitazaletin@yandex.ru>
2022-08-21 07:35:31 +0300

    test without perenos stroki

--- scripts/release.sh
+++ scripts/release.sh
@@ -44,10 +44,11 @@ DESCRIPTION="${COMMITS}"
 TEXT_A="123"
 TEXT_B="${TEXT_A} \n 456"
 
-echo "$(git log --pretty=format:"%H %an %s", "v_0.0.70"..."${RELEASE_VERSION}")" > in.txt
+#echo "$(git log --pretty=format:"%H %an %s", "v_0.0.70"..."${RELEASE_VERSION}")" > in.txt
+COMMITS=$(git log --pretty=format:"%H %an %s", "v_0.0.70"..."${RELEASE_VERSION}")
 #cat in.txt | tr -s ',' '\n' > out.txt
 #TEXT_B=$(cat in.txt)
-#echo "$COMMITS" > in.txt
+echo "$COMMITS" > in.txt
 #cat in.txt | tr -s ',' 'df' > out.txt
 COMMITS=$(cat in.txt)
 
