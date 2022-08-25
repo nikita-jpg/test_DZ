fe36528e6bcc045bf696f9d40e99d602fbeae399
nikit <nicitazaletin@yandex.ru>
2022-08-25 23:44:05 +0300

    test css in html

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+
 
 module.exports = {
   mode: "production",
@@ -27,11 +29,24 @@ module.exports = {
       { from: "./assets", to: "./assets" }
     ],
   }),
-    new CssMinimizerPlugin({
-        test: /\.css$/i,
-      }),
-    new HtmlMinimizerPlugin({
-        test: /\.html$/i,
-    }),
+    // new CssMinimizerPlugin({
+    //     test: /\.css$/i,
+    //   }),
+    // new HtmlMinimizerPlugin({
+    //     test: /\.html$/i,
+    // }),
+    new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, 'docs'),
+      src: 'index.html',
+      dest: 'index.html',
+      inline: true,
+      minify: true,
+      extract: true,
+      width: 375,
+      height: 565,
+      penthouse: {
+        blockJSRequests: false,
+      }
+    })
   ],
 };
