87128ce2d0fb320c2081639cdc72f320615ea1e8
nikit <nicitazaletin@yandex.ru>
2022-08-25 22:13:29 +0300

    test menu in css

--- index.js
+++ index.js
@@ -79,21 +79,7 @@
         });
     }
 
-    function makeMenu(node) {
-        let expanded = false;
-        const links = document.querySelector('.header__links');
-
-        node.addEventListener('click', () => {
-            expanded = !expanded;
-            node.setAttribute('aria-expanded', expanded ? 'true' : 'false');
-            node.querySelector('.header__menu-text').textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
-            links.classList.toggle('header__links_opened', expanded);
-            links.classList.add('header__links-toggled');
-        });
-    }
-
     document.addEventListener('DOMContentLoaded', () => {
         Array.from(document.querySelectorAll('.main__devices')).forEach(makeTabs);
-        // Array.from(document.querySelectorAll('.header__menu')).forEach(makeMenu);
     });
 })();
